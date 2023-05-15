e79d38145bc8d3854ca49eeafe2256559ad5e5db
Jay Que <jayque0378@gmail.com>
2023-05-15 21:44:36 +0000

    basic testcases

--- readme.md
+++ readme.md
@@ -5,5 +5,12 @@ Selenium Grid should be accessible
 http://localhost:4444/ui#/sessions 
 
 ## Run Scraper.Py 
-Look at video feed in Selenium Grid (Default password is 'Secret')
+Look at video feed in localhost. (Default password is 'Secret')
+
+---
+
+## Entering First Name // Last Name into Form
+Had trouble finding xPath.. used an extension to locate it. 
+The downside of using an exact path like this is it will run into an error if the webpage structure ever changes.
+https://addons.mozilla.org/en-US/firefox/addon/xpath_finder/
 
--- scraper.py
+++ scraper.py
@@ -1,24 +1,76 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.common.by import By
+import unittest
 
-print("Test Execution Started")
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-ssl-errors=yes')
-options.add_argument('--ignore-certificate-errors')
-driver = webdriver.Remote(
-command_executor='http://localhost:4444/wd/hub',
-options=options
-)
+# XPATH to elements on webpage 
+form_XPATH = "//*[@id=\"topfrm\"]"
+firstName_XPATH = "/html/body/div[1]/div[1]/div[3]/div[1]/div[2]/form/input[3]"
+lastName_XPATH = "/html/body/div[1]/div[1]/div[3]/div[1]/div[2]/form/input[4]"
+location_XPATH = "/html/body/div[1]/div[1]/div[3]/div[1]/div[2]/form/select" # location is a dropdown
+submit_XPATH = "/html/body/div[1]/div[1]/div[3]/div[1]/div[2]/form/button"
 
-#maximize the window size
-driver.maximize_window()
-time.sleep(10) # wait
-driver.get("https://golookup.com/")
+# Inputs to test
+firstName_input = ""
+lastName_input = ""    
+location_input = ""
 
-# https://selenium-python.readthedocs.io/locating-elements.html#locating-elements
+class GoLookupSearch(unittest.TestCase): 
+    def setUp(self):
+        print("Test Execution Started")
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-ssl-errors=yes')
+        options.add_argument('--ignore-certificate-errors')
+        self.driver = webdriver.Remote(
+        command_executor='http://localhost:4444/wd/hub',
+        options=options   
+        )
+        self.driver.get("https://golookup.com/")
 
+    # FORM
+    def testFormVisibility(self):
+        driver = self.driver
+        form = driver.find_element(By.XPATH, form_XPATH)
+        self.assertTrue(form.is_displayed(), "Form element not found")
 
-time.sleep(10) # wait
-driver.close()
-driver.quit()
-print("Test Execution Successfully Completed!")
+    # FIRST NAME
+    def testFirstNameVisibility(self):
+        firstName_Form = self.driver.find_element(By.XPATH, firstName_XPATH)
+        self.assertTrue(firstName_Form.is_displayed(), "First Name element not found")
+
+    def testEnterFirstName(self):
+        firstName_Form = self.driver.find_element(By.XPATH, firstName_XPATH)
+        firstName_Form.send_keys("Jane")
+        self.assertEqual(firstName_Form.get_attribute("value"), lastName_input, "Incorrect First Name entered")
+
+    # LAST NAME
+    def testLastNameVisibility(self):
+        firstName_Form = self.driver.find_element(By.XPATH, lastName_XPATH)
+        self.assertTrue(firstName_Form.is_displayed(), "Last Name element not found")
+
+    def testEnterLastName(self):
+        firstName_Form = self.driver.find_element(By.XPATH, lastName_XPATH)
+        firstName_Form.send_keys(lastName_input)
+        self.assertEqual(firstName_Form.get_attribute(lastName_input), lastName_input, "Incorrect Last Name entered")
+
+    # LOCATION
+    def testLocationVisibility(self):
+        location_Form = self.driver.find_element(By.XPATH, location_XPATH)
+        self.assertTrue(location_Form.is_displayed(), "Location element not found")
+
+    def testEnterLocation(self):
+        location_Form = self.driver.find_element(By.XPATH, location_XPATH)
+        location_Form.click()
+        option = f'//option[text()="{location_input}"]'
+        location_option = self.driver.find_element(By.XPATH, option)
+        self.assertEqual(location_option.get_attribute(location_input))
+        
+    def testSumbission(self):
+        submit_Form = self.driver.find_element(By.XPATH, submit_XPATH)
+        submit_Form.click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == "__main__":
+    unittest.main()
+    
